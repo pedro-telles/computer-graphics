852c3e3a552ddf1ec1fb4afc13cfa405a36d0e15
pedro-telles <54916464+pedro-telles@users.noreply.github.com>
2022-02-06 21:45:35 -0300

    Update parabola.py

--- OpenGL/Solidos de Revolução/parabola.py
+++ OpenGL/Solidos de Revolução/parabola.py
@@ -6,7 +6,7 @@ from math import *
 density = 15
 rotation_speed = 2
 
-def setColor(i, j): 
+def setColor(j): 
     
     if j < density//2: 
         r = (j - 255) / (density/2 - 1)
@@ -19,7 +19,7 @@ def setColor(i, j):
     
     glColor3f(r, g, b)
 
-def second_degree_function(u, v):
+def func_2_degree (u, v):
     theta = (v * 2 * pi) / (density - 1)
     w = (u * 2 / (density - 1))
 
@@ -29,7 +29,7 @@ def second_degree_function(u, v):
 
     return x, y, z
 
-def third_degree_function(u, v):
+def func_3_degree (u, v):
     theta = (v * 2 * pi) / (density - 1)
     w = ((u * 4) / (density - 1)) - 2
 
@@ -47,7 +47,7 @@ def draw_2_degree_points ():
     glBegin(GL_POINTS)
     for i in range(density):
         for j in range(density):
-            x, y, z = second_degree_function(i,j)
+            x, y, z = func_2_degree(i,j)
             glVertex3f(x, -y, z)
     glEnd()
 
@@ -58,9 +58,9 @@ def draw_2_degree_filled ():
     for i in range(density):
         glBegin(GL_TRIANGLE_STRIP)
         for j in range(density):
-            setColor(i, j)
-            glVertex3fv(second_degree_function(i,j))
-            glVertex3fv(second_degree_function(i - 1,j))
+            setColor(j)
+            glVertex3fv(func_2_degree(i,j))
+            glVertex3fv(func_2_degree(i - 1,j))
         glEnd()
 
 def draw_3_degree_filled ():
@@ -70,9 +70,9 @@ def draw_3_degree_filled ():
     for i in range(1,density):
         glBegin(GL_TRIANGLE_STRIP)
         for j in range(density):
-            setColor(i, j)
-            glVertex3fv(third_degree_function(i,j))
-            glVertex3fv(third_degree_function(i - 1,j))
+            setColor(j)
+            glVertex3fv(func_3_degree(i,j))
+            glVertex3fv(func_3_degree(i - 1,j))
         glEnd()
 
 angle = 0
